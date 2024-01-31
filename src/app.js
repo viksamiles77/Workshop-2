21e45ca877a01fde11801538e78f598143031d71
ErtanAlj <149390270+ErtanAlj@users.noreply.github.com>
2024-01-31 20:04:18 +0100

    test

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const url = "https://swapi.dev/api/";
-let peopleUrl = `${url}people/?page=<PAGE_NUMBER>`;
-let shipsUrl = `${url}starships/?page=<PAGE_NUMBER>`;
+let peopleUrl = `${url}people/?page=1`;
+let shipsUrl = `${url}starships/?page=1`;
 
 //  function get info (fetch)
 //  function show info table
@@ -26,31 +26,47 @@ function getPeople() {
   return fetch(peopleUrl)
     .then((res) => res.json())
     .then((people) => {
-      peopleFromApi = people; // store the people in a variable
-      return people; // return people
+      peopleFromApi = people.results; // store the people in a variable
     })
     .catch((error) => {
       console.log("ERROR", error);
     });
 }
+getPeople();
 
-function showPeople(people) {
+function getShips() {
+  // get ships func
+  return fetch(shipsUrl)
+    .then((res) => res.json())
+    .then((ships) => {
+      shipsFromApi = ships.results;
+    })
+    .catch((error) => {
+      console.log("ERROR", error);
+    });
+}
+getShips();
+
+function showPeople(event) {
+  loader.style.display = "block";
   let tableRows = "";
-  people.forEach((people) => {
+  console.log(peopleFromApi);
+  peopleFromApi.forEach((person) => {
     tableRows += `
     <tr>
-    <td>${people.name}</td>
-    <td>${people.height}</td>
-    <td>${people.mass}</td>
-    <td>${people.gender}</td>
-    <td>${people.birth_year}</td>
-    <td>${people.films.length}</td>
+    <td>${person.name}</td>
+    <td>${person.height}</td>
+    <td>${person.mass}</td>
+    <td>${person.gender}</td>
+    <td>${person.birth_year}</td>
+    <td>${person.films.length}</td>
     </tr>
     `;
   });
 
-  return `
-    <table>
+  resultDiv.innerHTML = "";
+  resultDiv.innerHTML = `
+    <table class="table-css">
     <tr>
     <th>Name</th>
     <th>Height</th>
@@ -64,14 +80,37 @@ function showPeople(people) {
     `;
 }
 
-showPeople();
+function showShips(event) {
+  let tableRows = "";
+  console.log(shipsFromApi);
+  shipsFromApi.forEach((ship) => {
+    tableRows += `
+    <tr>
+    <td>${ship.name}</td>
+    <td>${ship.model}</td>
+    <td>${ship.manufacturer}</td>
+    <td>${ship.cost_in_credits}</td>
+    <td>${ship.cargo_capacity}</td>
+    <td>${ship.starship_class}</td>
+    </tr>
+    `;
+  });
 
-function getShips() {
-  // get ships func
-  return fetch("https://swapi.dev/api/starships/")
-    .then((res) => res.json())
-    .then((ships) => {
-      shipsFromApi = ships;
-      return ships;
-    });
+  resultDiv.innerHTML = "";
+  resultDiv.innerHTML = `
+    <table class="table-css">
+    <tr>
+    <th>Name</th>
+    <th>Model</th>
+    <th>Manufacturer</th>
+    <th>Cost</th>
+    <th>People Capacity</th>
+    <th>Class</th>
+    </tr>
+    ${tableRows}  
+    </table>
+    `;
 }
+
+peopleBtn.addEventListener("click", showPeople);
+shipsBtn.addEventListener("click", showShips);
